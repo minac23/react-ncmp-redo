40893bf2491f8a1ce673d8b00e05af6791ff73f3
Celine Mina <ccmina23@gmail.com>
2022-05-02 15:56:23 -0700

    Completed Task 1 and Task 2 of Workshop #1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './App.css';
+import { Container, Navbar, NavbarBrand } from 'reactstrap';
+import NucampLogo from './app/assets/img/logo.png';
+import CampsitesList from './features/CampsitesList';
 
 function App() {
   return (
     <div className="App">
-      hello nucamp!
+        <Navbar dark color='primary' sticky='top' expand='md'>
+          <Container>
+            <NavbarBrand href='/'>
+              <img src={NucampLogo} alt='nucamp logo' />
+            </NavbarBrand>
+          </Container>
+        </Navbar>
+        <CampsitesList />
     </div>
   );
 }
