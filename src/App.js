36786f1d2edde46a00c07025544a120abec43c12
Celine Mina <ccmina23@gmail.com>
2022-06-02 10:54:48 -0700

    CampsiteDetailPage with useParams

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import CampsiteDetailPage from './pages/CampsiteDetailPage';
 
 function App() {
   return (
@@ -14,10 +15,14 @@ function App() {
           <Route path='/' element = {<HomePage />} />
           <Route path='contact' element={<ContactPage />} />
           <Route path='directory' element={<CampsitesDirectoryPage />} />
+          <Route
+            path='directory/:campsiteId'
+            element={<CampsiteDetailPage />}
+          />
       </Routes>
       <Footer />
     </div>
   );
-}
+};
 
 export default App;
--- src/pages/CampsitesDirectoryPage.js
+++ src/pages/CampsitesDirectoryPage.js
@@ -1,25 +1,15 @@
-import { useState } from 'react';
-import { Container, Row, Col } from 'reactstrap';
-import CampsiteDetail from '../features/campsites/CampsiteDetail';
+import { Container } from 'reactstrap';
 import CampsitesList from '../features/campsites/CampsitesList';
-import { selectCampsiteById } from '../features/campsites/campsitesSlice';
+
 
 const CampsitesDirectoryPage = () => {
    
-    const [ campsiteId, setCampsiteId ] = useState(0);
-    const selectCampsite = selectCampsiteById(campsiteId); // CampsiteDetail expects an object an not only its id. Not hooked, instead set value to campsite object returned from calling selectCampsiteById(campsiteId) -- campsiteId is hooked
+    // const [ campsiteId, setCampsiteId ] = useState(0);
+    // const selectCampsite = selectCampsiteById(campsiteId); // CampsiteDetail expects an object an not only its id. Not hooked, instead set value to campsite object returned from calling selectCampsiteById(campsiteId) -- campsiteId is hooked
 
     return (
         <Container>
-            
-            <Row>
-                <Col sm='5' md='7'>
-                    <CampsitesList setCampsiteId={setCampsiteId} />
-                </Col>
-                <Col sm='7' md ='5'>
-                    <CampsiteDetail campsite = {selectCampsite} />
-                </Col>
-            </Row>
+            <CampsitesList />
         </Container>
     );
 };
